5504d112b8f7fbf54bb989afa962136c5fc45bbb
Diego Santos <santos.diegoferreira@gmail.com>
2015-10-16 18:01:53 -0300

    Correção no código de teste.

--- owail-sender/src/test/java/br/org/owail/samples/SimpleGmail.java
+++ owail-sender/src/test/java/br/org/owail/samples/SimpleGmail.java
@@ -16,10 +16,9 @@ public class SimpleGmail {
 	mailer.setFrom(new Sender("Sender Name", "gmail.account@address.com", "the_password"));
 	
 	/* Configure email recipients */
-	mailer.addRecipient(Recipient.createTO("DevGroup", "desenvolvimento-elsa@googlegroups.com")); 	// main
-	mailer.addRecipient(Recipient.createCC("Diogo Ferreira", "diogo.rosas.ferreira@gmail.com")); 	// carbon copy
-	mailer.addRecipient(Recipient.createCC("Fagner Schwalm", "fagnerschwalm@gmail.com")); 		// carbon copy
-	mailer.addRecipient(Recipient.createBCC("Diego Santos", "santos.diegoferreira@gmail.com")); 	// blind carbon copy
+	mailer.addRecipient(Recipient.createTO("Recipient TO", "recipient.to@domain.com")); 	// main
+	mailer.addRecipient(Recipient.createCC("Recipient CC", "recipient.cc@domain.com")); 	// carbon copy
+	mailer.addRecipient(Recipient.createBCC("Recipient BCC", "recipient.bcc@domain.com")); 	// blind carbon copy
 	
 	/* Configure the email message */
 	mailer.setSubject("Simple Gmail Test!");
